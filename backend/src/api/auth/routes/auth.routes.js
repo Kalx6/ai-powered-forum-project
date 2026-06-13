d32fce23ea48417f4ec61b8fc78bbd0c68d12fba
suud-abrar <suudabrar0@gmail.com>
2026-06-13 11:59:34 +0300

    Orginazed all tasks together

--- backend/src/api/auth/routes/auth.routes.js
+++ backend/src/api/auth/routes/auth.routes.js
@@ -1,13 +1,14 @@
-import express from 'express';
+import express from "express";
 import {
   registerController,
   loginController,
-} from '../controller/auth.controller.js';
+  logoutController,
+} from "../controller/auth.controller.js";
 import {
   registerValidation,
   loginValidation,
-} from '../validations/auth.validation.js';
-
+} from "../validations/auth.validation.js";
+import { authenticateUser } from "../../../middleware/authentication.js";
 const router = express.Router();
 
 /**
@@ -15,13 +16,13 @@ const router = express.Router();
  * @desc Register a new user
  * @access Public
  */
-router.post('/register', registerValidation, registerController);
+router.post("/register", registerValidation, registerController);
 
 /**
  * @route POST /api/auth/login
  * @desc Authenticate user and get token
  * @access Public
  */
-router.post('/login', loginValidation, loginController);
-
+router.post("/login", loginValidation, loginController);
+router.post("/logout", authenticateUser, logoutController);
 export default router;
